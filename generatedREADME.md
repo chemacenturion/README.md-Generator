a77e7a9cee5f7f8d4dfb5b1d0df00c1416ac43a5
chemacenturion <centurion.philadelphia@gmail.com>
2021-08-05 10:55:39 -0400

    troubleshooting apache license

--- generatedREADME.md
+++ generatedREADME.md
@@ -1,5 +1,5 @@
 
-## README.md Generator ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+## README.md Generator 
 
 ## Description
 This project centers around the task of generating a README.md inside the terminal with node.js and inquirer prompts.
@@ -20,9 +20,9 @@ npm install
 Allow a user to generate a clean professional README.md quickly within the terminal using node.js.
 
 ## License
-MIT Copyright: Jose Centurion 2021
+Apache 2.0 Copyright: Jose Centurion 2021
 <br/>
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: <br/> The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. <br/> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+undefined
 
 ## Contributing
 No outside contributions for this project are being accepted at this time.
--- index.js
+++ index.js
@@ -108,7 +108,7 @@ Please reach out to me with any further questions via email at:
 <br/>
 ${answer.email}
 <br/>
-You can reach me via my contact info listed below:
+You can follow my work at my Github listed below:
 <br/>
 ${answer.username}
 
@@ -145,7 +145,7 @@ ${answer.username}
     }
 
     function licenseText(license) {
-    const mitLicense = 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: <br/> The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. <br/> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'
+    const mitLicense = 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: <br/> <br/> The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. <br/> <br/> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'
 
     const apacheLicense = 'Licensed under the Apache License, Version 2.0 (the "License"); <br/> you may not use this file except in compliance with the License. <br/> You may obtain a copy of the License at: <br/> http://www.apache.org/licenses/LICENSE-2.0 <br/> Unless required by applicable law or agreed to in writing, software <br/> distributed under the License is distributed on an "AS IS" BASIS, <br/> WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. <br/> See the License for the specific language governing permissions and <br/> limitations under the License.'
         
